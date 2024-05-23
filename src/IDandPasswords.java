a5db4e21a0cf077db94bcbbbfcb90b534625f92d
Brooks TreRon <PBU4685@hca.corpad.net>
2024-05-23 11:39:50 -0400

    Start of Interface creation

--- src/IDandPasswords.java
+++ src/IDandPasswords.java
@@ -1,4 +1,17 @@
+import java.util.HashMap;
 
 public class IDandPasswords {
+	
+	HashMap<String, String> logininf = new HashMap<>();
+	
+	public IDandPasswords() {
+		logininf.put("Bro", "Pizza");
+		logininf.put("Brometheus", "PASSWORD");
+		logininf.put("BroCode", "abc123");
+	}
+	
+	protected HashMap getInfo() {
+		return logininf;
+	}
 
 }
--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,4 +1,34 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
 
-public class LoginPage {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class LoginPage implements ActionListener {
+	
+	JFrame frame = new JFrame();
+	JButton loginbut = new JButton("Login");
+	JButton resetbut = new JButton("Reset");
+	JTextField userID = new JTextField();
+	JPasswordField userPass = new JPasswordField();
+	JLabel userIdLabel = new JLabel("userID:");
+	JLabel userpassLabel = new JLabel("password:");
+	HashMap<String,String> logininfo = new HashMap<>();
+	
+	public LoginPage(HashMap<String,String> map) {
+		
+		logininfo = map;
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
