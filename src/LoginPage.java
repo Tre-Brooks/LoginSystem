c69651544ef04408cc730d1e8ebb8db052848000
Brooks TreRon <PBU4685@hca.corpad.net>
2024-05-24 10:49:44 -0400

    Interface cotd

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -1,3 +1,4 @@
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
@@ -17,17 +18,50 @@ public class LoginPage implements ActionListener {
 	JPasswordField userPass = new JPasswordField();
 	JLabel userIdLabel = new JLabel("userID:");
 	JLabel userpassLabel = new JLabel("password:");
+	JLabel message = new JLabel();
 	HashMap<String,String> logininfo = new HashMap<>();
 	
 	public LoginPage(HashMap<String,String> map) {
 		
 		logininfo = map;
 		
+		userIdLabel.setBounds(50,100,75,25);
+		userpassLabel.setBounds(50,150,75, 25);
+		
+		message.setBounds(125,250,250,35);
+		message.setFont(new Font(null, Font.ITALIC,25));
+		
+		
+		userID.setBounds(125, 100 , 200, 25);
+		userPass.setBounds(125, 150 , 200, 25);
+		
+		loginbut.setBounds(125 , 200, 100, 25);
+		loginbut.addActionListener(this);
+		
+		resetbut.setBounds(225 , 200, 100, 25);
+		resetbut.addActionListener(this);
+		
+		frame.add(userIdLabel);
+		frame.add(userpassLabel);
+		frame.add(message);
+		frame.add(userID);
+		frame.add(userPass);
+		frame.add(loginbut);
+		frame.add(resetbut);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420, 420);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+			
+		if(e.getSource() == resetbut) {
+			userID.setText("");
+			userPass.setText("");
+		}
 		
 	}
 
